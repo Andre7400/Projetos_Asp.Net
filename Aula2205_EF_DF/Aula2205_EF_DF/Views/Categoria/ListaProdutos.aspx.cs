fde7eaeecaacca8f86eb9097c41a7e0407e10da0
1600535 <1600535@045LAB04012>
2017-05-22 11:42:01 -0300

    Data Base First

---
+++ Aula2205_EF_DF/Aula2205_EF_DF/Views/Categoria/ListaProdutos.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Aula2205_EF_DF.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2205_EF_DF.Views.Categoria
+{
+    public partial class ListaProdutos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            strConnLojaEntities contexto = new strConnLojaEntities();
+
+            grwProdutos.DataSource = contexto.Produtoes.ToList();
+            grwProdutos.DataBind();
+
+        }
+    }
+}
