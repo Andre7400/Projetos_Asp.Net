daf7e264072b21acaffa59933ae021830c9ace6a
1600535 <1600535@045LAB04012>
2017-06-07 10:25:27 -0300

    Criando Projeto MVC básico

---
+++ Aula0706_MVC/Aula0706_MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula0706_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Olá Mundo!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
