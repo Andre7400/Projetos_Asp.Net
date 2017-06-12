3ac42f8cb6e9978989801f7438d2ef28ebef2bea
1600535 <1600535@045LAB04012>
2017-06-12 10:57:49 -0300

    Adicionado Create , CategoriasController

--- Aula1206/Aula1206/Controllers/CategoriasController.cs
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Aula1206.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,18 +13,36 @@ namespace Aula1206.Controllers
         public ActionResult Index()
         {
 
-            List<string> categorias = new List<string>();
+           // List<string> categorias = new List<string>();
+            List<Categoria> categorias = new List<Categoria>();
 
-            categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Barcos");
-            categorias.Add("Aviões");
-            categorias.Add("Caminhões");
+
+                 categorias.Add(new Categoria() { Nome = "Carros" , Descricao = "Super Carros de coleção"});
+                 categorias.Add(new Categoria() { Nome = "Motos", Descricao = "As Motos maneiras de coleção" });
+                 categorias.Add(new Categoria() { Nome = "Barcos", Descricao = "Barcos com Turbina de coleção" });
+                 categorias.Add(new Categoria() { Nome = "Aviões", Descricao = "Aviões com  Carros de coleção" });
+                 categorias.Add(new Categoria() { Nome = "Caminhões", Descricao = "Caminhões com Carros de coleção" });
+           // categorias.Add("Carros");
+           // categorias.Add("Motos");
+           // categorias.Add("Barcos");
+           // categorias.Add("Aviões");
+           // categorias.Add("Caminhões");
 
             ViewBag.ListaCategorias = categorias;
 
-            return View();
+            return View(categorias);
 
+        }
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            return View(categoria);
         }
     }
 }
