622139cefaeaa5189e5e01b10f1e46f78804ca4a
1600535 <1600535@045LAB04013>
2017-06-19 11:46:15 -0300

    Adicionando Projeto (Editar, excluir ,views ) automatico com layout

---
+++ Wingtiptoys_MVC/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseModels
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage ="*"), StringLength(10,ErrorMessage ="Tamanho máximo de 10 cacteres")]
+        public string Descricao { get; set; }
+        [Display(Name ="Descricao"), DataType(DataType.MultilineText)]
+        public string Nome { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
